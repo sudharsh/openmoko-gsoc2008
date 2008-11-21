77d141ec9c71fbc8e9d35e49e06cfa0906f650d8
Sudharshan 'Sup3rkiddo' S <sudharsh@gmail.com>
2008-11-22 00:25:02 +0530

    Search for power_supply substring in the netlink buffer the right way

--- fsod/src/subsystems/Device/power/power-helpers.c
+++ fsod/src/subsystems/Device/power/power-helpers.c
@@ -43,7 +43,7 @@ static gboolean process_uevent (GIOChannel *source, GIOCondition *condition, Pow
 
 	recv(sockfd, &buf, sizeof(buf), 0);
 	/* Sneaky little way of checking if 'power_supply' is present in `buf' */
-	if (g_strsplit(buf, "power_supply", 0) && g_strcmp0(_status, curr_status)) { 
+	if (g_strstr_len(buf, -1, "power_supply") && g_strcmp0(_status, curr_status)) { 
 		g_message ("Battery status changed to %s", _status);
 		g_signal_emit_by_name(self, "power_status", _status);
 		power_set_curr_status(self, _status);
